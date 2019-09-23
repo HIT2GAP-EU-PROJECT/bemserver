046059473812a830a447ec463069c4f91bc95ee3
BONNAMY Pierre <pibonnamy@nobatek.com>
2019-09-23 19:00:54 +0200

    Fix regex. Handle ONTOLOGY_BASE_URL variable.

--- deployment/scripts/update-settings.sh
+++ deployment/scripts/update-settings.sh
@@ -3,7 +3,7 @@ set -e
 
 # Update settings files from environment variables.
 # Get environment variables beginning with PATTERN and update or create values in CONFIG_FILE
-# Usage: ./update-settings.sh "BEMSRV_" "/config/settings.cfg"
+# Usage: /scripts/update-settings.sh "BEMSRV_" "${FLASK_SETTINGS_FILE}"
 #
 
 CONFIG_FILE=$2
@@ -13,12 +13,15 @@ CUTVAL=$((${#PATTERN}+1))
 # Update key, value parameters in CONFIG_FILE.
 # If key exist update value, if not create key=value pair.
 function update_file(){
-    regexp="^$1=\(.*\)"
-    if [ -z $(grep "$regexp" $CONFIG_FILE) ]
+    regexp="^$1\s?=.*"
+    # echo "PATTERN: $regexp"
+    if [ -z $(grep -E "$regexp" $CONFIG_FILE) ]
     then
-        echo "$1 = '$2'" >> $CONFIG_FILE
+        # echo "NEW VALUE: $1='$2'"
+        echo "$1='$2'" >> $CONFIG_FILE
     else
-        sed -i "s#$regexp#$1 = '$2'#g" $CONFIG_FILE
+        # echo "UPDATE: $1='$2'"
+        sed -i -E "s#$regexp#$1='$2'#g" $CONFIG_FILE
     fi
 }
 
@@ -29,6 +32,11 @@ printenv | grep ^"${PATTERN}" | cut -c ${CUTVAL}- | while read -r line ; do
     IFS="=" read -ra parts <<< "$line"
     update_file "${parts[0]}" "${parts[1]}"
 done
+
+# Specific treatment for ONTOLOGY_BASE_URL
+if [ ! -z ${ONTOLOGY_BASE_URL} ]; then
+    update_file "ONTOLOGY_BASE_URL" "${ONTOLOGY_BASE_URL}"
+fi
 
 echo "Update success"
 echo "-----------------------------------"
