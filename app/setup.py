42a0cfabe1d820e7bb1fac13131edd26c7eb856e
David FREDERIQUE <dfrederique@nobatek.com>
2019-09-26 18:15:49 +0200

    Upgrade and limit all dependencies + Complete setup attributes

--- app/setup.py
+++ app/setup.py
@@ -18,13 +18,13 @@ with open(path.join(_SETUP_PATH, 'README.md'), encoding='utf-8') as f:
 setup(
     name='bemserver',
     version='0.0.1',
-    description='Hit2gap platform Core',
+    description='BEMServer API',
     long_description=_LONG_DESCRIPTION,
-    # url='https://github.com/pypa/sampleproject',
+    url='https://bemserver.org',
     # Author details
     author='Nobatek/INEF4',
-    #author_email='pypa-dev@googlegroups.com',
-    #license='',
+    author_email='contact@bemserver.org',
+    license='GNU Affero General Public License',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
@@ -33,15 +33,16 @@ setup(
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
         # Indicate who your project is intended for
-        #'Intended Audience :: Developers',
-        #'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
         # Pick your license as you wish (should match "license" above)
-        #'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
     # What does your project relate to?
-    #keywords='sample setuptools development',
+    keywords='building energy management api',
     packages=find_packages(exclude=['tests*']),
 
     package_data={
@@ -54,21 +55,21 @@ setup(
         'flask-rest-api>=0.10.0,<0.11.0',
         'webargs>=5.1.3,<6.0',
         'apispec>=0.39.0,<1.0.0',
-        'python-dateutil>=2.5.0',
+        'python-dateutil>=2.5.0,<2.9.0',
         'marshmallow>=2.15.2,<3.0.0',
         'marshmallow-oneofschema>=1.0.5,<2.0.0',
-        'requests>=2.11.1',
-        'numpy==1.14.0',
-        'pandas<0.25',
-        'sparqlwrapper',
+        'requests>=2.22.0,<2.23.0',
+        'numpy>=1.14.0,<1.18.0',
+        'pandas>=0.25.0,<0.26.0',
+        'sparqlwrapper>=1.8.4,<1.9.0',
         'flask-jwt-simple>=0.0.3,<0.1.0',
         'python3-saml>=1.4.1,<1.5',
-        'tables>=3.3.0',
+        'tables>=3.3.0,<3.6.0',
         'pint>0.7,<0.9',
-        'sqlalchemy>=1.2.5',
-        'sqlalchemy-utils>=0.32.21',
-        'flask-sqlalchemy>=2.3.2',
-        'flask-migrate>=2.1.1',
-        'python-dotenv>=0.8.2'
+        'sqlalchemy>=1.2.5,<1.4.0',
+        'sqlalchemy-utils>=0.32.21,<0.35.0',
+        'flask-sqlalchemy>=2.3.2,<2.5.0',
+        'flask-migrate>=2.1.1,<2.6.0',
+        'python-dotenv>=0.8.2,<0.11.0'
     ],
 )
